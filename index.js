821ecb40bfb38b3587540816c595702f7164290a
tgs <eatgnues90@gmail.com>
2021-05-01 07:47:37 +0900

    EDIT menu_btn active by scroll

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 // }
 
 //menu_btn
-const menu_btn_array = document.getElementsByClassName('menu_btn');
+let menu_btn_array = document.getElementsByClassName('menu_btn');
 
 //page별 scroll top 높이
 const pageHeight = document.documentElement.clientHeight;
@@ -23,12 +23,12 @@ const page5Height = pageHeight * 4;
 const page6Height = pageHeight * 5;
 
 //스크롤 시
-const printed_page = 1;
+let printed_page = 1;
+menu_btn_change_active(1);
 window.addEventListener('scroll', () => {
 
     // 현재 스크롤바 위치
 	let scrollLocation = document.documentElement.scrollTop;
-    console.log(scrollLocation);
 
     if(scrollLocation >= page1Height && printed_page !== 1){ menu_btn_change_active(1); }
     if(scrollLocation >= page2Height && printed_page !== 2){ menu_btn_change_active(2); }
@@ -49,4 +49,8 @@ window.addEventListener('scroll', () => {
 function menu_btn_change_active(num){
     console.log('menu_btn_change_active' + num);
     printed_page = num;
+    for (const menu_btn of menu_btn_array) {
+        menu_btn.classList.remove('active');
+    }
+    menu_btn_array[num-1].classList.add('active');
 }
