63d74b8908fa5e6c556b0aee39c8cdf210e4376c
tgs <eatgnues90@gmail.com>
2021-05-10 23:27:12 +0900

    edit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
+const content = document.querySelector('#content');
+const br = document.createElement("br");
 
 
 /* 메뉴 호버 시 드롭다운 */
@@ -9,8 +11,7 @@
             menu_title[i].parentNode.querySelector('ul').classList.add('act');
         });
     }
-
-    const content = document.querySelector('#content');
+    
     content.addEventListener('mouseover', function(){
         remove_now_act();
     });
@@ -23,45 +24,31 @@
     }
 
 
-
 /* 스크롤 이벤트 */
     history.scrollRestoration = "manual"; //리로드시 최상단
 
+    //section 순서대로 css 설정
+    const sections = content.querySelectorAll('section');
+    for(let i=0; i<sections.length; i++){
+        sections[i].style.top = `${100*i}vh`;
+    }
+
     //페이지별 scrolltop 값 저장
-    let nowPage = 'p1';
+    let nowPage = 'page1';
     const vhPage = window.innerHeight; //창 높이
     const saElementList = document.querySelectorAll('.page');
-    let pageTopArr = new Array();
-    for (const element of saElementList) {
-        pageTopArr.push(element.getBoundingClientRect().top);
+    let pageInfo = {}
+    let prePage = null;
+    for (let i=0; i<saElementList.length; i++) {
+        pageInfo[`page${i+1}`] = {}
+        pageInfo[`page${i+1}`].pre = prePage;
+        pageInfo[`page${i+1}`].top = saElementList[i].getBoundingClientRect().top;
+        let next = null;
+        if(saElementList.length >= i+2){ next = `page${i+2}`; }
+        pageInfo[`page${i+1}`].next = next;
+        prePage = `page${i+1}`;
     }
-    const pageInfo = {
-        p1:{
-            pre:null,
-            top:pageTopArr[0],
-            next:'p2'
-        },
-        p2:{
-            pre:'p1',
-            top:pageTopArr[1],
-            next:'p3'
-        },
-        p3:{
-            pre:'p2',
-            top:pageTopArr[2],
-            next:'p4'
-        },
-        p4:{
-            pre:'p3',
-            top:pageTopArr[3],
-            next:'p5'
-        },
-        p5:{
-            pre:'p4',
-            top:pageTopArr[4],
-            next:null
-        }
-    }
+
     //스크롤
     let initScrollY = null;
     let operating = false;
@@ -69,27 +56,20 @@
         initScrollY = e.touches[0].screenY;
     }
     const setScrollEvent = (e) => {
-        console.log(1);
-        
         if(!operating){
             operating = true;
                         
             //up, down 체크
             let dir;
             if(e.changedTouches){ //모바일
-                if(e.changedTouches[0].screenY < initScrollY){
-                    dir = 'down';
-                } 
-                if(e.changedTouches[0].screenY > initScrollY){
-                    dir = 'up';
-                }
+                if(e.changedTouches[0].screenY < initScrollY){ dir = 'down'; } 
+                if(e.changedTouches[0].screenY > initScrollY){ dir = 'up'; }
             } else { //pc
                 if(e.deltaY>0){ dir = 'down'; }
                 if(e.deltaY<0){ dir = 'up'; }
             }
 
             //실행
-            const content = document.querySelector('#content');
             if(dir === 'down'){
                 if(pageInfo[nowPage].next !== null) {
                     content.classList.add(`move_${pageInfo[nowPage].next}`);
@@ -104,14 +84,16 @@
                     nowPage = pageInfo[nowPage].pre;
                 }
             }
-            console.log(2);
-
-            setTimeout(function(){ operating = false; }, 1300);
+            setTimeout(function(){
+                operating = false;
+                pageAniStart(nowPage);
+            }, 1500);
         }
     }
     window.addEventListener('wheel', setScrollEvent); //pc
-    window.addEventListener('touchstart', saveScrollStart);
-    window.addEventListener('touchend', setScrollEvent);
+    window.addEventListener('touchstart', saveScrollStart); //mo
+    window.addEventListener('touchend', setScrollEvent); //mo
+
 
 /* page1 */
     //로고
@@ -126,8 +108,6 @@
     },1500);
     //글씨
     setTimeout(function(){
-        const br = document.createElement("br");
-
         const hu_txt = document.querySelectorAll('#page1 .hu_txt > li');
         const tw_title = document.querySelector('.tw-title');
         tw_title.classList.remove('s');
@@ -154,56 +134,24 @@
             typingIdx++;
         },180);
     }, 2000);
+
+
 /* page2 */
-
-/* 스크롤 페이지 이동 */
-    
-    
-    // //스크롤 시 글씨 나타나는 효과
-    // const saTriggerMargin = 300;
-    // const saElementList = document.querySelectorAll('.page');
-
-    // const saFunc = function() {
-    //     for (const element of saElementList) {
-    //         if (!element.classList.contains('show')) {
-    //             if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
-    //                 element.classList.add('show');
-    //             }
-    //         }
-    //     }
-    // }
-
-    // window.addEventListener('scroll', saFunc);
-
-
-//시작
-
-
-// const myDropdown = document.querySelector('#language_btn')
-// myDropdown.addEventListener('mouseover', function () {
-//     document.querySelector('.dropdown-menu').show();
-// })
-
-
-// //스크롤
-//     menu_btn_change_active(1);
-//     window.addEventListener('scroll', function(){
-
-//         //메뉴 active 여부 체크
-//         let scrollLocation = document.documentElement.scrollTop;
-//         if(scrollLocation >= page1Height && scrollLocation < page2Height && printed_menu !== 1){ menu_btn_change_active(1); }
-//         if(scrollLocation >= page2Height && scrollLocation < page3Height && printed_menu !== 2){ menu_btn_change_active(2); }
-//         if(scrollLocation >= page3Height && scrollLocation < page6Height && printed_menu !== 3){ menu_btn_change_active(3); }
-//         if(scrollLocation >= page6Height && printed_menu !== 4){ menu_btn_change_active(4); }
-
-//         //페이지별 애니메이션
-
-//     });
-
-    
-// //scroll에 따라 nav menu 버튼 활성화
-//     function menu_btn_change_active(num){
-//         menuBtns.map(function(id){ document.querySelector(id).classList.remove('active'); }); //active 모두 삭제
-//         document.querySelector(menuBtns[num-1]).classList.add('active'); //active 추가
-//         printed_menu = num; //현재 활성화 값 저장
-//     }
+    const pageAniStart = (page) => {
+        //배경이미지
+        const page_bg = document.querySelector(`#${page} .bg`);
+        if(page_bg === null) { return false; }
+        setTimeout(function(){
+            console.log(page);
+            if(page_bg.classList.contains('s')){ page_bg.classList.remove('s'); }
+        },);
+        //글씨
+        setTimeout(function(){
+            const title = document.querySelector(`#${page} .desc2 > .title`);
+            if(title.classList.contains('s')){ title.classList.remove('s'); }
+        }, 1000);
+        setTimeout(function(){
+            const body = document.querySelector(`#${page} .desc2 > .body`);
+            if(body.classList.contains('s')){ body.classList.remove('s'); }
+        }, 2000);
+    }
